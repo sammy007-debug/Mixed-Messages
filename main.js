0fe12f5fe692a3b21290197b01edb6c3adcef855
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-16 20:26:49 +0100

    Refactor code & correct spelling errors

--- main.js
+++ main.js
@@ -4,50 +4,50 @@
 
 const messages = {
     _compliments: [
-        'You have a great sense of humor.', 
-        'You have impeccable manners.', 
+        'You have a great sense of humor', 
+        'You have impeccable manners', 
         'Is that your picture next to "charming" in the dictionary?', 
-        'You are beautiful on the inside and outside.', 
-        'You were cool way before hipsters were cool.', 
-        'That thing you don\'t like about yourself is what makes you really interesting.', 
-        'You\'re inspiring.', 
-        'I bet you do crossword puzzles in ink.', 
-        'You\'re an awesome friend.', 
-        'You look great today.', 
-        'That color is perfect on you.', 
+        'You are beautiful on the inside and outside', 
+        'You were cool way before hipsters were cool', 
+        'That thing you don\'t like about yourself is what makes you really interesting', 
+        'You\'re inspiring', 
+        'I bet you do crossword puzzles in ink', 
+        'You\'re an awesome friend', 
+        'You look great today', 
+        'That color is perfect on you', 
         'Has anyone ever told you that you have great posture?', 
-        'You\'re one of a kind.', 
-        'In high school, I bet you were voted "most likely to continue being awesome."', 
+        'You\'re one of a kind', 
+        'In high school, I bet you were voted "most likely to continue being awesome"', 
         'You look so young!', 
-        'You are stunning.', 
-        'You are one of the bravest people I know.'
+        'You are stunning', 
+        'You are one of the bravest people I know'
     ],
     get compliments () {
-        return this._compliments ? this._compliments : null
+        return this._compliments
     },
     _fortunes: [ // Disclaimer, these predictions are for entertainment purposes and should not be taken seriously
         // affirmative answers
-        'It is certain.',
-        'Without a doubt.',
-        'You may rely on it.',
-        'Most likely.',
-        'Outcome is good.',
-        'Yes.',
+        'It is certain',
+        'Without a doubt',
+        'You may rely on it',
+        'Most likely',
+        'Outcome is good',
+        'Yes',
         //neutral answers
-        'Reply hazy, try again.',
-        'Ask again later.',
-        'Better not tell you now.',
-        'Cannot predict now.',
-        'Concentrate and ask again.',
+        'Reply hazy, try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
         // negative answers
-        'Don\'t count on it.',
-        'My reply is no.',
-        'My sources say no.',
-        'Very doubtful.',
-        'Outlook not so good.'
+        'Don\'t count on it',
+        'My reply is no',
+        'My sources say no',
+        'Very doubtful',
+        'Outlook not so good'
     ],
     get fortunes () {
-        return this._predictions ? this._predictions : null
+        return this._fortunes
     },
     _activities: [
         'Make homemade ice cream',
@@ -62,36 +62,36 @@ const messages = {
         'Write a short story'
     ],
     get activities () {
-        return this._activities ? this._activities : null
+        return this._activities
     },
     _quotes: [
         {
             author: 'Mahatma Gandhi',
-            quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever.'
+            quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever'
         },
         {
             author: 'Friedrich Nietzsche',
-            quote: 'That which does not kill us makes us stronger.'
+            quote: 'That which does not kill us makes us stronger'
         },
         {
             author: 'Bernard M. Baruch',
-            quote: 'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.'
+            quote: 'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind'
         },
         {
             author: 'Albert Einstein',
-            quote: 'Strive not to be a success, but rather to be of value.'
+            quote: 'Strive not to be a success, but rather to be of value'
         },
         {
             author: 'Maya Angelou', 
-            quote: 'I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.'
+            quote: 'I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel'
         },
         {
             author: 'Amelia Earhart',
-            quote: 'The most difficult thing is the decision to act, the rest is merely tenacity.'
+            quote: 'The most difficult thing is the decision to act, the rest is merely tenacity'
         },
         {
             author: 'Charles Swindoll',
-            quote: 'Life is 10% what happens to me and 90% of how I react to it. '
+            quote: 'Life is 10% what happens to me and 90% of how I react to i'
         },
         {
             author: 'Christopher Columbus',
@@ -99,47 +99,47 @@ const messages = {
         },
         {
             author: 'Unknown',
-            quote: 'You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.'
+            quote: 'You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground'
         },
         {
             author: 'Steve Jobs',
-            quote: 'The only way to do great work is to love what you do.'
+            quote: 'The only way to do great work is to love what you do'
         }
     ],
     get quotes () {
-        return this._quotes ? this._quotes : null
+        return this._quotes
     },
     _jokes: [
         'You sound like you could survive a zombie apocalypse :)', 
-        'Why can’t a bike stand on its own? It’s two tired.', 
-        'I submitted ten puns to a pun contest hoping that one would win, but no pun in ten did.', 
-        'I told my doctor that I broke my arm in two places. He told me to stop going to those places.', 
-        'You don\'t need a parachute to go skydiving — you need a parachute to go skydiving twice.', 
-        'A nurse told me, "Sorry for the wait!" I replied, "It\'s alright, I\'m patient."', 
-        'Working in a mirror factory is something I could totally see myself doing.', 
+        'Why can’t a bike stand on its own? It’s two tired', 
+        'I submitted ten puns to a pun contest hoping that one would win, but no pun in ten did', 
+        'I told my doctor that I broke my arm in two places. He told me to stop going to those places', 
+        'You don\'t need a parachute to go skydiving — you need a parachute to go skydiving twice', 
+        'A nurse told me, "Sorry for the wait!" I replied, "It\'s alright, I\'m patient"', 
+        'Working in a mirror factory is something I could totally see myself doing', 
         'What do you call a cheese that’s not yours ... Nacho cheese!', 
-        'What’s orange and sounds like a parrot? ... A carrot.', 
-        'Why does Humpty Dumpty love autumn? ... Because he always has a great fall.', 
-        'Where to spaghetti and sauce go to dance? ... The meat ball.', 
-        'When the bottle of Pepsi hit me, I didn\'t cry. It was a soft drink.'
+        'What’s orange and sounds like a parrot? ... A carrot', 
+        'Why does Humpty Dumpty love autumn? ... Because he always has a great fall', 
+        'Where to spaghetti and sauce go to dance? ... The meat ball', 
+        'When the bottle of Pepsi hit me, I didn\'t cry. It was a soft drink'
     ],
     get jokes () {
-        return this._jokes ? this._jokes : null
+        return this._jokes
     },
     random(messageType) { // this method works irrespective of the array length
         // make sure the randomIndex isn't more than the indeces in the array
-        let max = this['_' + messageType].length
+        let max = this[messageType].length
         let randomIndex = Math.floor(Math.random() * max)
 
         // return the random message based on the message type
-        return this['_' + messageType][randomIndex]
+        return this[messageType][randomIndex]
     }
 }
 
 // list random messages of all message types
-console.log(messages.random('compliments'))
-console.log(messages.random('fortunes'))
-console.log(messages.random('activities'))
+console.log('Compliment:',messages.random('compliments'))
+console.log('Fortune:',messages.random('fortunes'))
+console.log('Activity:',messages.random('activities'))
 let quoteObj = messages.random('quotes')  // call the random method on quotes once so that the quote and author are of the same quote
-console.log(quoteObj.quote + ' ~ ' + quoteObj.author)
-console.log(messages.random('jokes'))
+console.log('Quote:',quoteObj.quote + ' ~ ' + quoteObj.author)
+console.log('Joke:',messages.random('jokes'))
