1e4e8b4eed44d0b773c3e5132b101371dc3f9d00
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-19 18:53:40 +0100

    Code static web page v1.0

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ const messages = {
         },
         {
             author: 'Charles Swindoll',
-            quote: 'Life is 10% what happens to me and 90% of how I react to i'
+            quote: 'Life is 10% what happens to me and 90% of how I react to it'
         },
         {
             author: 'Christopher Columbus',
