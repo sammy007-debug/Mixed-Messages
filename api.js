e009551f7f21d1749c7914a383242653f9f30936
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-20 16:15:30 +0100

    Add html data attributes to the tabs & refactor api.js

--- api.js
+++ api.js
@@ -1,10 +1,8 @@
-const displayedMessage = document.querySelector('.text')
-
 function getActivity() {
     fetch('https://www.boredapi.com/api/activity/')
     .then(res => res.json())
-    .then(data => {console.log('Activity: ' + data.activity)})
-    .catch(error => {console.error('Error: ' + error)})
+    .then(data => console.log('Activity: ' + data.activity))
+    .catch(error => console.error('Error: ' + error))
 }
 
 function getJoke() {
@@ -16,23 +14,21 @@ function getJoke() {
 
     fetch('https://icanhazdadjoke.com/', options)
     .then(res => res.json())
-    .then(data => {console.log('Joke: ' + data.joke)})
-    .catch(error => {console.error('Error: ' + error)})
+    .then(data => console.log('Joke: ' + data.joke))
+    .catch(error => console.error('Error: ' + error))
 }
 
 function getCompliment() {
     fetch('https://complimentr.com/api')
     .then(res => res.json())
-    .then(data => {console.log(displayedMessage.innerHTML = data.compliment)})
-    .catch(error => {console.error('Error: ' + error)})
+    .then(data => displayedMessage.innerHTML = data.compliment)
+    .catch(error => console.error('Error: ' + error))
 }
 
 // we don't have a good API for quotes yet!!!
 function getQuote() {
     fetch('https://inspiration.goprogram.ai/')
     .then(res => res)
-    .then(data => {console.log('Quote: ' + data.quote + ' ~ ' + data.author)})
-    .catch(error => {console.error('Error: ' + error)})
+    .then(data => console.log('Quote: ' + data.quote + ' ~ ' + data.author))
+    .catch(error => console.error('Error: ' + error))
 }
-
-export { getActivity, getCompliment, getJoke }
